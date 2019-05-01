818fada969cf86dc44f50440b80d8400ba83b557
大森夢拓 <daishinmutaku@gmail.com>
2019-05-01 19:20:52 +0900

    paper最終調整

--- kentei.py
+++ kentei.py
@@ -1,5 +1,6 @@
 import math
 
+import mglearn
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_boston
@@ -12,6 +13,9 @@ from sklearn.model_selection import train_test_split
 def main():
 
     # サンプルデータを用意
+    # data = mglearn.datasets
+    # X, y = data.load_extended_boston()
+
     dataset = load_boston()
     # 標本データを取得
     X = dataset.data
@@ -23,13 +27,13 @@ def main():
     print("y: %dのベクトル" % y.shape)
 
     # データの詳細
-    print("目的変数：ボストンに建てられた住宅価格の中央値")
-    X_columns = dataset.feature_names
-    i = 0
-    print("各カラムの構成")
-    for column in X_columns:
-        print("β%d：%s" % (i, column))
-        i += 1
+    # print("目的変数：ボストンに建てられた住宅価格の中央値")
+    # X_columns = dataset.feature_names
+    # i = 0
+    # print("各カラムの構成")
+    # for column in X_columns:
+    #     print("β%d：%s" % (i, column))
+    #     i += 1
 
     # 重み
     poly = PolynomialFeatures(degree=X.shape[1])
@@ -45,8 +49,10 @@ def main():
     lr_pl = Pipeline([("poly_10", poly), ("lr", lr)])
     print("バイアス: %f" % lr_pl.named_steps.lr.intercept_)
     i = 0
+    lrBeta = []
     for beta in lr_pl.named_steps.lr.coef_:
         print("β%d: %0.3f" % (i, beta))
+        lrBeta.append(beta)
         i += 1
     plt.xlabel("Coefficient index")
     plt.ylabel("Coefficient magnitude")
@@ -58,22 +64,23 @@ def main():
     # ridge model
 
     # crossvalidation
-    scaler = StandardScaler()
-    crf = RidgeCV(alphas=10 ** np.arange(-2, 2, 0.1), cv=5)
-    scaler.fit(X)
-    crf.fit(scaler.transform(X), y)
-    ridgeBestAlpha = crf.alpha_
-    print('最適なλ：%f' % ridgeBestAlpha)
+    # scaler = StandardScaler()
+    # crf = RidgeCV(alphas=10 ** np.arange(-2, 2, 0.1), cv=5)
+    # scaler.fit(X)
+    # crf.fit(scaler.transform(X), y)
+    # ridgeBestAlpha = crf.alpha_
+    # print('最適なλ：%f' % ridgeBestAlpha)
 
 
     print("\n ridge model")
     ridgeAlpha = []
     for alpha in alphaConf:
-        if ridgeBestAlpha < alpha and alpha/10 < ridgeBestAlpha:
-            ridgeAlpha.append(ridgeBestAlpha)
+        # if ridgeBestAlpha < alpha and alpha/10 < ridgeBestAlpha:
+        #     ridgeAlpha.append(ridgeBestAlpha)
         ridgeAlpha.append(alpha)
     ridgeTrainSetScore = []
     ridgeTestSetScore = []
+    ridgeBeta = []
     for alpha in ridgeAlpha:
         ridge = Ridge(alpha=alpha).fit(X_train, y_train)
         print("\n alpha={}".format(str(alpha)))
@@ -85,6 +92,7 @@ def main():
         ridge_pl = Pipeline([("poly_10", poly), ("ridge", ridge)])
         print("バイアス: %f" % ridge_pl.named_steps.ridge.intercept_)
         i = 0
+        ridgeBeta.append(ridge_pl.named_steps.ridge.coef_)
         for beta in ridge_pl.named_steps.ridge.coef_:
             print("β%d: %0.3f" % (i, beta))
             i += 1
@@ -98,18 +106,18 @@ def main():
     # lasso model
 
     # crossvalidation
-    scaler = StandardScaler()
-    clf = LassoCV(alphas=10 ** np.arange(-2, 2, 0.1), cv=5)
-    scaler.fit(X)
-    clf.fit(scaler.transform(X), y)
-    lassoBestAlpha = clf.alpha_
-    print('最適なλ：%f' % lassoBestAlpha)
+    # scaler = StandardScaler()
+    # clf = LassoCV(alphas=10 ** np.arange(-2, 2, 0.1), cv=5)
+    # scaler.fit(X)
+    # clf.fit(scaler.transform(X), y)
+    # lassoBestAlpha = clf.alpha_
+    # print('最適なλ：%f' % lassoBestAlpha)
 
     print("\n lasso model")
     lassoAlpha = []
     for alpha in alphaConf:
-        if lassoBestAlpha < alpha and alpha/10 < lassoBestAlpha :
-            lassoAlpha.append(lassoBestAlpha)
+        # if lassoBestAlpha < alpha and alpha/10 < lassoBestAlpha :
+        #     lassoAlpha.append(lassoBestAlpha)
         lassoAlpha.append(alpha)
     lassoTrainSetScore = []
     lassoTestSetScore = []
@@ -152,25 +160,55 @@ def main():
     plt.plot(alphaLog, lassoTestSetScore, label="lasso_test")
     plt.xlabel("log_10(α)")
     plt.ylabel("determination R^2")
-    plt.plot([math.log10(ridgeBestAlpha), math.log10(ridgeBestAlpha)], [0, 1], "black", linestyle='dashed', label="ridge_CV")
-    plt.plot([math.log10(lassoBestAlpha), math.log10(lassoBestAlpha)], [0, 1], "gray", linestyle='dashed', label="lasso_CV")
+    # plt.plot([math.log10(ridgeBestAlpha), math.log10(ridgeBestAlpha)], [0, 1], "black", linestyle='dashed', label="ridge_CV")
+    # plt.plot([math.log10(lassoBestAlpha), math.log10(lassoBestAlpha)], [0, 1], "gray", linestyle='dashed', label="lasso_CV")
     plt.legend()
     plt.savefig('score.pdf', transparent=True)
     plt.show()
 
-    # solution path of lasso
-    i = 0
-    lassoBetaArray = np.array(lassoBeta)
-    lassoBetaArrayT = lassoBetaArray.T
-    for beta in lassoBetaArrayT:
+    # solution path of linear
+    i = 1
+    lrBetaArray = np.array(lrBeta)
+    lrBetaArrayT = lrBetaArray.T
+    for beta in lrBetaArrayT:
+        print(beta)
+        plt.plot([0, 1], [beta, beta], label="β:{:.0f}".format(i))
+        i += 1
+    plt.ylabel('β')
+    plt.title('Least Squares Path')
+    plt.legend()
+    # plt.savefig('lrPath.pdf', transparent=True)
+    plt.show()
+
+    # solution path of ridge
+    i = 1
+    ridgeBetaArray = np.array(ridgeBeta)
+    ridgeBetaArrayT = ridgeBetaArray.T
+    for beta in ridgeBetaArrayT:
         print(beta)
         plt.plot(alphaLog, beta, label="β:{:.0f}".format(i))
         i += 1
     plt.xlabel('log_10(α)')
-    plt.ylabel('beta')
+    plt.ylabel('β')
+    plt.title('Ridge Path')
+    plt.legend()
+    # plt.savefig('ridgePath.pdf', transparent=True)
+    plt.show()
+
+    # solution path of lasso
+    i = 1
+    lassoBetaArray = np.array(lassoBeta)
+    lassoBetaArrayT = lassoBetaArray.T
+    for beta in lassoBetaArrayT:
+        print("β:{:.0f}".format(i))
+        print(beta)
+        plt.plot(alphaLog, beta, label="β:{:.0f}".format(i))
+        i += 1
+    plt.xlabel('log_10(α)')
+    plt.ylabel('β')
     plt.title('LASSO Path')
     plt.legend()
-    plt.savefig('lassoPath.pdf', transparent=True)
+    # plt.savefig('lassoPath.pdf', transparent=True)
     plt.show()
 
 
